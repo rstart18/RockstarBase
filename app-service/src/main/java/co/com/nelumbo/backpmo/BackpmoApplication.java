719d98d7d94890d67fcc7ea4c1f48610ea75e397
Sebastian Baez <Sebastian.Baez@segurossura.com.mx>
2025-07-04 10:16:31 -0500

    'newbase'

---
+++ app-service/src/main/java/co/com/nelumbo/backpmo/BackpmoApplication.java
@@ -0,0 +1,13 @@
+package co.com.nelumbo.backpmo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackpmoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackpmoApplication.class, args);
+	}
+
+}
