568cfa0dd97d508f7d7f5f5831e200064a964590
rstart18 <40774070+rstart18@users.noreply.github.com>
2025-09-22 15:40:53 -0500

    Add global exception handling

---
+++ infrastructure/src/main/java/co/com/nelumbo/backpmo/infrastructure/exception/NotFoundException.java
@@ -0,0 +1,7 @@
+package co.com.nelumbo.backpmo.infrastructure.exception;
+
+public class NotFoundException extends RuntimeException {
+    public NotFoundException(String message) {
+        super(message);
+    }
+}
